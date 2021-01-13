6e33b8a638e55d3a74dde4984d7478375fa28319
anbunambi3108 <anbunambi3108@gmail.com>
2021-01-13 13:00:01 +0530

    flutter truecaller signin

---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_truecaller/flutter_truecaller.dart';
+import 'package:new_app/verify_mobile_number.dart';
+import 'package:new_app/home_page.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: MyApp(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final FlutterTruecaller truecaller = FlutterTruecaller();
+
+  @override
+  void initState() {
+    super.initState();
+    getTrueCaller();
+  }
+
+  Future getTrueCaller() async {
+    await truecaller.initializeSDK(
+      sdkOptions: FlutterTruecallerScope.SDK_OPTION_WITH_OTP,
+      footerType: FlutterTruecallerScope.FOOTER_TYPE_ANOTHER_METHOD,
+      consentMode: FlutterTruecallerScope.CONSENT_MODE_POPUP,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xff6ab7de),
+        // centerTitle: true,
+        title: Text("Truecaller Flutter App"),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Center(
+                  child: Text("Login Or Singup Using",
+                      style: TextStyle(fontSize: 20))),
+              SizedBox(height: 20),
+              RaisedButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
+                color: Color(0xff6ab7de),
+                onPressed: () async {
+                  await truecaller.getProfile();
+                  FlutterTruecaller.manualVerificationRequired
+                      .listen((isRequired) {
+                    if (isRequired) {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => VerifyMobileNumber()));
+                    } else {
+                      Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(builder: (context) => HomePage()));
+                    }
+                  });
+                },
+                child: Container(
+                    height: 50,
+                    child: Center(
+                        child: Text('Mobile Number'.toUpperCase(),
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 18)))),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
